9dddeeec09b88e68a4e21c460a7ad538b7d91d56
Skiif64 <skiif64@yandex.ru>
2023-01-20 23:18:44 +0400

    add base types

--- src/FSMTeleBot/Filters/FilterAttribute.cs
+++ src/FSMTeleBot/Filters/FilterAttribute.cs
@@ -2,19 +2,13 @@
 
 public class FilterAttribute : Attribute
 {
-    public string? ContainsCommand { get; set; }
-    public string? Contains { get; set; }
-    public string? Regexp { get; set; }
-    public bool RequireAdmin { get; set; }
+    public string? ContainsCommand { get; init; }
+    public string? Contains { get; init; }
+    public string? Regexp { get; init; }
+    public bool RequireAdmin { get; init; }
 
-    public FilterAttribute(string? containsCommand = null,
-        string? contains = null,
-        string? regexp = null,
-        bool requireAdmin = false)
+    public FilterAttribute()
     {
-        ContainsCommand = containsCommand;
-        Contains = contains;
-        Regexp = regexp;
-        RequireAdmin = requireAdmin;
+        
     }
 }
---
+++ src/FSMTeleBot/IHandlersStorage.cs
@@ -0,0 +1,9 @@
+﻿using FSMTeleBot.Handlers.Abstractions;
+using Telegram.Bot.Types;
+
+namespace FSMTeleBot;
+
+public interface IHandlersStorage
+{
+    IHandler<T>? GetHandler<T>(T argument) where T : Message;
+}

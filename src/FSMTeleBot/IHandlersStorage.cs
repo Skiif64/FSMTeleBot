c45bb12b043cc26b390054ace3b48bfbc2176726
Skiif64 <skiif64@yandex.ru>
2023-01-24 18:26:52 +0400

    UPD added Register method to IHandlersStorage

--- src/FSMTeleBot/IHandlersStorage.cs
+++ src/FSMTeleBot/IHandlersStorage.cs
@@ -1,9 +1,11 @@
 ﻿using FSMTeleBot.Handlers.Abstractions;
+using System.Reflection;
 using Telegram.Bot.Types;
 
 namespace FSMTeleBot;
 
 public interface IHandlersStorage
 {
+    void Register(Assembly assembly);
     IHandler<T>? GetHandler<T>(T argument) where T : Message;
 }

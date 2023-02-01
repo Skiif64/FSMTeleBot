002d7bcfad4c729d15dd406ebdea77aacee5fbe3
Алексей Байрышевский <s3mods@yandex.ru>
2023-02-01 16:48:12 +0400

    Test for StateGroupTests

---
+++ test/FSMTeleBot.Tests/FSM/StateGroupTests.cs
@@ -0,0 +1,32 @@
+﻿using FSMTeleBot.FSM;
+using Moq;
+
+namespace FSMTeleBot.Tests.FSM;
+
+public class StateGroupTests
+{    
+    class TestStateGroup : StateGroup
+    {
+        public static IState State1 { get; }
+        public static IState State2 { get; }
+        public static IState State3 { get; }
+        public TestStateGroup()
+        {
+            InitState(this);
+        }
+    }
+    private readonly IStateGroup _stateGroup;
+
+    public StateGroupTests()
+    {       
+        _stateGroup = new TestStateGroup();
+    }
+
+    [Test]
+    public void WhenCreated_Then3StatesShouldContain()
+    {
+        Assert.IsNotNull(_stateGroup.States);
+        Assert.That(_stateGroup.States.Count, Is.EqualTo(3));
+        CollectionAssert.AllItemsAreInstancesOfType(_stateGroup.States, typeof(IState));        
+    }
+}

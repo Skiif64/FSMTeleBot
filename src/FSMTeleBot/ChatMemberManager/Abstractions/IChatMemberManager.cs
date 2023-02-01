6e27e75284b3fdddbd238067d5a6f65a08fc6954
Алексей Байрышевский <s3mods@yandex.ru>
2023-02-01 11:00:23 +0400

    Added ChatMemberManager

---
+++ src/FSMTeleBot/ChatMemberManager/Abstractions/IChatMemberManager.cs
@@ -0,0 +1,8 @@
+﻿using Telegram.Bot.Types.Enums;
+
+namespace FSMTeleBot.ChatMemberManager.Abstractions;
+
+public interface IChatMemberManager
+{
+    Task<ChatMemberStatus> GetChatMemberStatus(long chatId, long userId, CancellationToken cancellationToken = default);
+}
---
+++ src/FSMTeleBot/ChatMemberManager/ChatMemberManager.cs
@@ -0,0 +1,21 @@
+﻿using FSMTeleBot.ChatMemberManager.Abstractions;
+using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+
+namespace FSMTeleBot.ChatMemberManager;
+
+public class ChatMemberManager : IChatMemberManager
+{
+    private readonly ITelegramBotClient _client;
+
+    public ChatMemberManager(ITelegramBotClient client)
+    {
+        _client = client;
+    }
+
+    public virtual async Task<ChatMemberStatus> GetChatMemberStatus(long chatId, long userId, CancellationToken cancellationToken = default)
+    {
+        var member = await _client.GetChatMemberAsync(chatId, userId, cancellationToken);
+        return member.Status;
+    }
+}

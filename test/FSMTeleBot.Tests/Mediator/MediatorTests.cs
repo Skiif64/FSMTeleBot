bbdfa6debda901a4546392029de08d67b409e76d
Skiif64 <skiif64@yandex.ru>
2023-02-12 14:46:58 +0400

    Fixed Mediator test

--- test/FSMTeleBot.Tests/Mediator/MediatorTests.cs
+++ test/FSMTeleBot.Tests/Mediator/MediatorTests.cs
@@ -3,28 +3,34 @@ using FSMTeleBot.Handlers.Abstractions;
 using FSMTeleBot.Internal.Mediator;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
+using System.Collections.Generic;
 using System.ComponentModel;
 using Telegram.Bot.Types;
 
 namespace FSMTeleBot.Tests.Mediator;
 
 public class MediatorTests
-{    
+{
     private readonly IServiceProvider _serviceProvider;
     private readonly IBotMediator _mediator;
     private readonly Mock<IHandler<Message>> _startHandlerMock;
     private readonly Mock<IHandler<Message>> _cancelHandlerMock;
 
     public MediatorTests()
-    {       
-        _serviceProvider = SetupServiceProvider();
-        _mediator = new BotMediator(_serviceProvider);
-
-        _startHandlerMock = new Mock<IHandler<Message>>();        
+    {
+        _startHandlerMock = new Mock<IHandler<Message>>();
         TypeDescriptor.AddAttributes(_startHandlerMock.Object, new MessageFilterAttribute { Contains = "start" });
 
-        _cancelHandlerMock = new Mock<IHandler<Message>>();        
+        _cancelHandlerMock = new Mock<IHandler<Message>>();
         TypeDescriptor.AddAttributes(_cancelHandlerMock.Object, new MessageFilterAttribute { Contains = "cancel" });
+
+        var serviceProviderMock = new Mock<IServiceProvider>();
+        serviceProviderMock
+            .Setup(x => x.GetService(typeof(IEnumerable<IHandler<Message>>)))
+        .Returns(new[] { _startHandlerMock.Object, _cancelHandlerMock.Object });
+
+        _serviceProvider = serviceProviderMock.Object;
+        _mediator = new BotMediator(_serviceProvider);
     }
 
     [SetUp]
@@ -38,16 +44,7 @@ public class MediatorTests
         _cancelHandlerMock
             .Setup(x => x.HandleAsync(It.IsAny<Message>(), default))
             .Returns(Task.CompletedTask);
-    }
-
-    private IServiceProvider SetupServiceProvider()
-    {
-        var serviceCollection = new ServiceCollection();
-
-        serviceCollection.AddTransient<IHandler<Message>>(_ => _cancelHandlerMock.Object);
-        serviceCollection.AddTransient<IHandler<Message>>(_ => _startHandlerMock.Object);
-        return serviceCollection.BuildServiceProvider();
-    }
+    }    
 
     [Test]
     public void When_SendStartMessage_Then_HandleCorrectHandler()

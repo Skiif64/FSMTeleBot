49d8cc9d9418cf73ce568dca40fb14f6ec6b571e
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-07 11:27:00 +0400

    Added BotBuilderTests

---
+++ test/FSMTeleBot.Tests/BotBuilderConfigurationTests.cs
@@ -0,0 +1,34 @@
+﻿using Telegram.Bot;
+
+namespace FSMTeleBot.Tests;
+
+public class BotBuilderConfigurationTests
+{
+    private BotBuilderConfiguration _configuraton = null!;
+
+	public BotBuilderConfigurationTests()
+	{
+
+	}
+
+	[SetUp]
+	public void SetupConfiguration()
+	{
+		_configuraton = new BotBuilderConfiguration();
+	}
+
+	[Test]
+	public void WhenUseCustomTelegramBot_TelegramBotType_ThenShouldNotFall()
+	{
+		var type = typeof(TelegramBotClient);
+
+		Assert.DoesNotThrow(() => _configuraton.UseCustomTelegramBotClient(type));
+	}
+
+    [Test]
+    public void WhenUseCustomTelegramBot_StringType_ThenShouldThrowArgumentException()
+    {
+        var type = typeof(string);		
+        Assert.Throws<ArgumentException>(() => _configuraton.UseCustomTelegramBotClient(type));
+    }
+}

565687ed9fdb6fe0c67837347489fbd4c2e4ceef
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-14 14:03:31 +0400

    Added HandlerBase

---
+++ src/FSMTeleBot/Handlers/Abstractions/HandlerBase`1.cs
@@ -0,0 +1,24 @@
+﻿using FSMTeleBot.States.Abstractions;
+using Telegram.Bot;
+
+namespace FSMTeleBot.Handlers.Abstractions;
+
+public class HandlerBase<TMessage> : IHandler<TMessage> //TODO: implement
+{
+    private readonly IChatContextFactory _factory;
+    private IChatContext _context;
+    protected ITelegramBotClient Client { get; }
+    //protected IChatContext Context => _context ??=
+        //_factory.GetContextAsync().Result; //TODO: refactor
+
+    public HandlerBase(ITelegramBotClient client, IChatContextFactory contextFactory)
+    {
+        Client = client;
+        _factory = contextFactory;            
+    }
+
+    public Task HandleAsync(TMessage data, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}

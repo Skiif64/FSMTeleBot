36a569740e9b665005b2e0b4a27d218d2cd9cc1e
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-14 16:47:27 +0400

    Added InitAsync method to HandlerBase and implemented to HandlerWrapper

--- src/FSMTeleBot/Handlers/Abstractions/HandlerBase`1.cs
+++ src/FSMTeleBot/Handlers/Abstractions/HandlerBase`1.cs
@@ -3,22 +3,25 @@ using Telegram.Bot;
 
 namespace FSMTeleBot.Handlers.Abstractions;
 
-public class HandlerBase<TMessage> : IHandler<TMessage> //TODO: implement
+public class HandlerBase<TMessage> : IHandler<TMessage>
 {
-    private readonly IChatContextFactory _factory;
-    private IChatContext _context;
+    private readonly IChatContextFactory _contextFactory;    
     protected ITelegramBotClient Client { get; }
-    //protected IChatContext Context => _context ??=
-        //_factory.GetContextAsync().Result; //TODO: refactor
+    protected IChatContext Context { get; private set; } = null!;
 
     public HandlerBase(ITelegramBotClient client, IChatContextFactory contextFactory)
     {
         Client = client;
-        _factory = contextFactory;            
+        _contextFactory = contextFactory;            
     }
 
     public Task HandleAsync(TMessage data, CancellationToken cancellationToken = default)
     {
         throw new NotImplementedException();
+    }
+
+    internal async Task InitAsync(TMessage data, CancellationToken cancellationToken = default)
+    {
+        Context = await _contextFactory.GetContextAsync(data, cancellationToken);
     }
 }

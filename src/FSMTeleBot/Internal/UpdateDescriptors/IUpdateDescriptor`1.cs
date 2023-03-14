d783fae49536bb6f7859b68654d6600bd2539286
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-14 15:21:29 +0400

    Added update descriptor

---
+++ src/FSMTeleBot/Internal/UpdateDescriptors/IUpdateDescriptor`1.cs
@@ -0,0 +1,9 @@
+﻿using Telegram.Bot.Types.Enums;
+
+namespace FSMTeleBot.Internal.UpdateDescriptors;
+
+internal interface IUpdateDescriptor<TMessage>
+{
+    long GetChatId(TMessage message);
+    long GetUserId(TMessage message);
+}
---
+++ src/FSMTeleBot/Internal/UpdateDescriptors/MessageDescriptor.cs
@@ -0,0 +1,10 @@
+﻿using Telegram.Bot.Types;
+
+namespace FSMTeleBot.Internal.UpdateDescriptors;
+
+internal class MessageDescriptor : IUpdateDescriptor<Message>
+{
+    public long GetChatId(Message message) => message.Chat.Id;
+
+    public long GetUserId(Message message) => message.From?.Id ?? throw new Exception(nameof(message)); //TODO: normal exception
+}

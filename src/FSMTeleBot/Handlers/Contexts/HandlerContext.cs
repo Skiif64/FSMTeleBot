6f45adbf04b9797e18fc7b84e36264336012f0b1
Алексей Байрышевский <s3mods@yandex.ru>
2023-04-25 20:15:07 +0400

    Added HandlerContext

---
+++ src/FSMTeleBot/Handlers/Contexts/HandlerContext.cs
@@ -0,0 +1,11 @@
+﻿using FSMTeleBot.Handlers.Abstractions;
+
+namespace FSMTeleBot.Handlers.Contexts;
+public class HandlerContext<TData> : IHandlerContext<TData>
+{
+    public TData Data { get; }
+    public HandlerContext(TData data)
+    {
+        Data = data;
+    }
+}

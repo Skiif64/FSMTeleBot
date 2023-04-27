a9be1d5b7a9512c587d09cac708ecaabc2c807c9
Алексей Байрышевский <s3mods@yandex.ru>
2023-04-27 12:16:13 +0400

    Added ICallbackSerializer

---
+++ src/FSMTeleBot/Callbacks/ICallbackSerializer.cs
@@ -0,0 +1,7 @@
+﻿namespace FSMTeleBot.Callbacks;
+public interface ICallbackSerializer
+{
+    CallbackInfo Deserialize(string callbackData);
+    T DeserializeAs<T>(string callbackData) where T: ICallbackQuery, new();
+    string Serialize<T>(T data) where T: ICallbackQuery, new();
+}

1812400fc5ae8a916772bb0988140bc3b4a5ee5e
Алексей Байрышевский <s3mods@yandex.ru>
2023-04-27 12:22:06 +0400

    Added CallbackSerializer

---
+++ src/FSMTeleBot/Callbacks/CallbackSerializer.cs
@@ -0,0 +1,54 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace FSMTeleBot.Callbacks;
+internal class CallbackSerializer : ICallbackSerializer
+{
+    private const string Separator = ";";
+
+    public CallbackInfo Deserialize(string callbackData)
+    {        
+        var args = callbackData.Split(Separator, StringSplitOptions.RemoveEmptyEntries);
+        var header = args[0];
+        var data = args[1..];
+        return new CallbackInfo(header, data);
+    }
+
+    public T DeserializeAs<T>(string callbackData) where T : ICallbackQuery, new()
+    {//TODO: Property caching?
+        var info = Deserialize(callbackData);
+        var type = typeof(T);
+
+        var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        var query = Activator.CreateInstance<T>();
+        var headerProperty = properties[0];
+        headerProperty.SetValue(query, info.Header);
+        var dataProperties = properties[1..];
+
+        int i = 0;
+        foreach (var property in dataProperties)
+        {//TODO: add value converters
+            property.SetValue(query, info.Data[i]);
+            i++;
+        }
+
+        return query;
+    }
+
+    public string Serialize<T>(T data) where T : ICallbackQuery, new()
+    {
+        var type = typeof(T);
+        var builder = new StringBuilder(64);
+        var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        var headerProperty = properties[0];
+        var dataProperties = properties[1..];
+        builder.Append(headerProperty.GetValue(data));
+        foreach(var property in dataProperties)
+        {
+            builder.Append(';');
+            builder.Append(property.GetValue(data));
+        }
+        return builder.ToString();
+    }
+}

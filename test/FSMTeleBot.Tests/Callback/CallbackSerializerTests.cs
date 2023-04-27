51643840628e3c9b6d624db3dd810a8c1f4473f6
Алексей Байрышевский <s3mods@yandex.ru>
2023-04-27 13:24:09 +0400

    Added CallbackSerializerTests

---
+++ test/FSMTeleBot.Tests/Callback/CallbackSerializerTests.cs
@@ -0,0 +1,58 @@
+﻿using FSMTeleBot.Callbacks;
+
+namespace FSMTeleBot.Tests.Callback;
+internal class CallbackSerializerTests
+{
+    private readonly CallbackSerializer _serializer;
+
+    public CallbackSerializerTests()
+    {
+        _serializer = new();
+    }
+
+    [Test]
+    public void WhenSerialize_TestQuery_ThenShouldReturnExpectedSerializedString()
+    {
+        var query = new TestCallbackQuery
+        {
+            Value1 = "1",
+            Value2 = "2",
+        };
+        var expected = "Test;1;2";
+
+        var actual = _serializer.Serialize(query);
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void WhenDeserialize_TestQueryString_ThenShouldReturnExpectedCallbackInfo()
+    {
+        var queryString = "Test;1;2";
+        var expected = new CallbackInfo("Test", new[] { "1", "2" });
+
+        var actual = _serializer.Deserialize(queryString);
+
+        Assert.That(actual.Header, Is.EqualTo(expected.Header));
+        CollectionAssert.AreEqual(expected.Data, actual.Data);
+    }
+    [Test]
+    public void WhenDeserializeAsTestCallbackQuery_TestQueryString_ThenShouldReturnTestCallbackQuery()
+    {
+        var queryString = "Test;1;2";
+        var expected = new TestCallbackQuery
+        {
+            Value1 = "1",
+            Value2 = "2",
+        };
+
+        var actual = _serializer.DeserializeAs<TestCallbackQuery>(queryString);
+        Assert.Multiple(() =>
+        {
+            Assert.That(actual, Is.Not.Null);
+            Assert.That(actual.Header, Is.EqualTo(expected.Header));
+            Assert.That(actual.Value1, Is.EqualTo(expected.Value1));
+            Assert.That(actual.Value2, Is.EqualTo(expected.Value2));
+        });
+
+    }
+}
---
+++ test/FSMTeleBot.Tests/Callback/TestCallbackQuery.cs
@@ -0,0 +1,10 @@
+﻿using FSMTeleBot.Callbacks;
+
+namespace FSMTeleBot.Tests.Callback;
+internal class TestCallbackQuery : ICallbackQuery
+{
+    public string Header { get; set; } = "Test"; //TODO: remove setter
+
+    public string Value1 { get; set; } = string.Empty;
+    public string Value2 { get; set; } = string.Empty;
+}

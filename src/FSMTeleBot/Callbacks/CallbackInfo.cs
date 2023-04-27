d2fb23f37fc159587bef1cda392b82c5a48bfbd0
Алексей Байрышевский <s3mods@yandex.ru>
2023-04-27 11:20:39 +0400

    Added CallbackInfo

---
+++ src/FSMTeleBot/Callbacks/CallbackInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+namespace FSMTeleBot.Callbacks;
+public class CallbackInfo : IEnumerable<string>
+{
+    public string Header { get; }
+    public string[] Data { get; }
+
+    internal CallbackInfo(string header, string[] data)
+    {
+        Header = header;
+        Data = data;
+    }
+
+    internal CallbackInfo(string header, IEnumerable<string> data)
+        : this(header, data.ToArray())
+    {
+
+    }
+
+    internal CallbackInfo(string header)
+        : this(header, Array.Empty<string>())
+    {
+
+    }
+    public IEnumerator<string> GetEnumerator()
+    {
+        foreach (var data in Data)
+        {
+            yield return data;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    => Data.GetEnumerator();
+
+}

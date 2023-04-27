6076fe1fdd2f7b3c186be70855196b5209f2dca7
Алексей Байрышевский <s3mods@yandex.ru>
2023-04-27 10:47:07 +0400

    Added Utils/MessageBuilder

---
+++ test/FSMTeleBot.Tests/Utils/MessageBuilder.cs
@@ -0,0 +1,42 @@
+﻿using Telegram.Bot.Types;
+
+namespace FSMTeleBot.Tests.Utils;
+internal class MessageBuilder
+{
+    public Message Message { get; }
+    private MessageBuilder()
+    {
+        Message = new Message
+        {
+            Text = string.Empty,            
+        };
+    }    
+
+    public static MessageBuilder Create() 
+        => new();
+
+    public MessageBuilder WithText(string text)
+    {
+        Message.Text = text;
+        return this;
+    }
+
+    public MessageBuilder WithChat(long chatId)
+    {
+        Message.Chat = new Chat
+        {
+            Id = chatId
+        };
+        return this;
+    }
+
+    public MessageBuilder WithUser(long userId)
+    {
+        Message.From = new User
+        {
+            Id = userId
+        };
+        return this;
+    }
+
+}

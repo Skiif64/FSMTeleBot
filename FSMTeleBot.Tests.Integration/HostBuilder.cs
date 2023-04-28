005b5bbd8b4a956421407cadb0693338cb2f75c1
Алексей Байрышевский <s3mods@yandex.ru>
2023-04-28 12:04:02 +0400

    Added base for tests

---
+++ FSMTeleBot.Tests.Integration/HostBuilder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSMTeleBot.Tests.Integration;
+internal abstract class HostBuilder
+{
+    public abstract IServiceProvider BuildServiceProvider();
+}
---
+++ FSMTeleBot.Tests.Integration/RecieveMessage/RecieveMessageTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using FSMTeleBot.Extensions;
+using dotenv.net;
+
+namespace FSMTeleBot.Tests.Integration.RecieveMessage;
+internal class RecieveMessageTests : IFix
+{
+}
+
+internal class RecieveMessageHostBuilder : HostBuilder
+{
+    public override IServiceProvider BuildServiceProvider()
+    {
+        var collection = new ServiceCollection();
+        collection.AddFSMTeleBot(config =>
+        {
+            config.AddAssemblyFrom<RecieveMessageHostBuilder>();
+            config.UseBuiltinOptions(new TelegramBotOptions(false,
+                new Telegram.Bot.TelegramBotClientOptions
+                {
+                    Token = 
+                }))
+        });
+    }
+}

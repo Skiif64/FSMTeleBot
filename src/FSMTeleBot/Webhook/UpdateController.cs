8c473c6431e7edabfafb2922adb50527ee2d3c90
Алексей Байрышевский <s3mods@yandex.ru>
2023-05-02 11:11:39 +0400

    Added Base for webhook

---
+++ src/FSMTeleBot/Webhook/UpdateController.cs
@@ -0,0 +1,14 @@
+﻿using EmbedIO;
+using EmbedIO.Routing;
+using EmbedIO.WebApi;
+using Telegram.Bot.Types;
+
+namespace FSMTeleBot.Webhook;
+public class UpdateController : WebApiController
+{
+    [Route(HttpVerbs.Post, "update")]
+    public async Task UpdateAsync()
+    {
+        var update = await HttpContext.GetRequestDataAsync<Update>();        
+    }
+}

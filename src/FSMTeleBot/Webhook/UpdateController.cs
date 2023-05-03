7db4b5b091f162fbf0bc55473867585b2ab6d444
Алексей Байрышевский <s3mods@yandex.ru>
2023-05-03 10:50:27 +0400

    Added UpdateHandler to UpdateController

--- src/FSMTeleBot/Webhook/UpdateController.cs
+++ src/FSMTeleBot/Webhook/UpdateController.cs
@@ -1,23 +1,30 @@
 ﻿using EmbedIO;
 using EmbedIO.Routing;
 using EmbedIO.WebApi;
+using FSMTeleBot.Services;
 using Newtonsoft.Json;
 using Telegram.Bot.Types;
 
 namespace FSMTeleBot.Webhook;
-public class UpdateController : WebApiController
+internal class UpdateController : WebApiController //TODO: make public?
 {
-    [Route(HttpVerbs.Post, "update")]
+    private readonly InternalUpdateHandler _updateHandler; //TODO: use interface
+
+    public UpdateController(InternalUpdateHandler updateHandler)
+    {
+        _updateHandler = updateHandler;
+    }
+
+    [Route(HttpVerbs.Post, "/update")]
     public async Task UpdateAsync()
     {       
         var update = await HttpContext.GetRequestDataAsync<Update>(DeserializeUpdateAsync);  
         if(update is null)
         {
             HttpContext.Response.StatusCode = 400;
-
             return;
         }
-        
+        await _updateHandler.HandleUpdateAsync(update, CancellationToken);
     }
 
     private static async Task<Update> DeserializeUpdateAsync(IHttpContext context) 
